f1ce018630e74a8ddc3a4a117a71f164287142cf
Jon Moore <jonrmoore@protonmail.com>
2018-10-02 19:48:18 -0500

    Negative numbers now work

--- NumberPalindrome/src/com/company/Main.java
+++ NumberPalindrome/src/com/company/Main.java
@@ -3,24 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getDigit(1234));
+        System.out.println(returnLastDigit(-1234));
+        System.out.println(returnNextToLastDigit(-1234));
         System.out.println(addDigitToReverse(32, 9));
         System.out.println(isPalindrome(454));
         System.out.println(isPalindrome(2345));
         System.out.println(isPalindrome(45));
         System.out.println(isPalindrome(927282729));
         System.out.println(isPalindrome(99));
+        System.out.println(isPalindrome(-222));
     }
 
     public static boolean isPalindrome(int number) {
         int reverse = 0;
         int initNum = number;
 
-        while (number > 0) {
-            int digit = number % 10;
+        while (number > 0 || number < 0) {
+            int digit = returnLastDigit(number);
             reverse = addDigitToReverse(reverse, digit);
             number /= 10;
-//            System.out.println("Changed number: " + number + "\nReverse: " + reverse + "\nInitial Value: " + initNum);
         }
 
         if (initNum == reverse) {
@@ -28,15 +29,25 @@ public class Main {
         }
 
         return false;
+
     }
 
-    public static int getDigit(int num) {
-        int digit = num % 10;
-        return digit;
+//    public static int returnNextToLastDigit (int num) {
+//
+//        num /= 10;
+//        int nextToLastDigit = num % 10;
+//        return nextToLastDigit;
+//    }
+
+    public static int returnLastDigit (int num) {
+        int lastDigit = num % 10;
+        return lastDigit;
     }
 
     public static int addDigitToReverse(int num, int newDig) {
         int numWithNewDigit = (num * 10) + newDig;
         return numWithNewDigit;
     }
+
+
 }
