295a42d0c587ff1e73135bc4bf42de211a5d23d1
Jon Moore <jonrmoore@protonmail.com>
2018-10-07 22:17:58 -0500

    Zeroes still not printing

--- NumberToWords/src/com/jonmoore/Main.java
+++ NumberToWords/src/com/jonmoore/Main.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         numberToWords(123);
+        numberToWords(001);
 //        System.out.println(reverse(7893));
     }
 
     public static void numberToWords(int num) {
         // reverse number
         int reversedNum = reverse(num);
+
+        int digitCount = getDigitCount(num);
+        int reversedDigitCount = getDigitCount(reversedNum);
 
         if (reversedNum < 0) {
             System.out.println("Invalid Value");
@@ -54,6 +58,10 @@ public class Main {
 
             reversedNum /= 10;
         }
+
+        for (int i = (digitCount - reversedDigitCount); i > 0; i--) {
+            System.out.println("Zero");
+        }
     }
 
     // Without reversing the input, the output prints in opposite order.
@@ -67,5 +75,23 @@ public class Main {
         }
 
         return reversedNum;
+    }
+
+    // To leading zeroes to print "zero" rather than nothing
+    private static int getDigitCount(int num) {
+        if (num < 0) {
+            return -1;
+        }
+
+        int count = 0;
+        for (int i = 0; i <= num; i *= 10) {
+            if (i == 0) {
+                i++;
+            }
+
+            count++;
+        }
+
+        return count;
     }
 }
