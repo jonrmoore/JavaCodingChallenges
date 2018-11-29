e97865dfc94b6c4881709702e6314d84ab0bf434
Jon Moore <jonmoore510@gmail.com>
2018-11-28 20:19:04 -0600

    User input challenge complete

--- ReadingUserInput/src/com/jonrmoore/Main.java
+++ ReadingUserInput/src/com/jonrmoore/Main.java
@@ -5,28 +5,48 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter your year of birth:");
+//        boolean hasInt = scanner.hasNextInt();
+//        int yearOfBirth = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        if (hasInt) {
+//            System.out.println("Enter your name:");
+//            String name = scanner.nextLine();
+//
+//            int age = 2018 - yearOfBirth;
+//
+//            if (age >= 0 && age <= 100) {
+//                System.out.println("Your name is " + name + " and you are " + age + " years of age.");
+//            } else {
+//                System.out.println("Invalid birth year");
+//            }
+//        } else {
+//            System.out.println("Please type a number");
+//        }
+//
+//        scanner.close();
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter your year of birth:");
-        boolean hasInt = scanner.hasNextInt();
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();
-
-        if (hasInt) {
-            System.out.println("Enter your name:");
-            String name = scanner.nextLine();
-
-            int age = 2018 - yearOfBirth;
-
-            if (age >= 0 && age <= 100) {
-                System.out.println("Your name is " + name + " and you are " + age + " years of age.");
+        int count = 0;
+        int sum = 0;
+        while (count != 10) {
+            System.out.println("Enter number #" + (count + 1) + ":");
+            boolean hasInt = scanner.hasNextInt();
+            if(hasInt) {
+                int input = scanner.nextInt();
+                System.out.println("You entered " + input);
+                sum += input;
+                count++;
             } else {
-                System.out.println("Invalid birth year");
+                System.out.println("Invalid number");
             }
-        } else {
-            System.out.println("Please type a number");
+
+            scanner.nextLine();
         }
 
+        System.out.println("Sum = " + sum);
         scanner.close();
     }
 }
